379927293ae8e39ea7793a1ec56babab84aaaf80
Pablo Pivatto <pepivatto@gmail.com>
2020-01-09 19:59:37 -0300

    primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,52 @@
+var socket = io();
+
+var params = new URLSearchParams( window.location.search );
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+    
+    socket.emit('entrarChat', usuario, function (resp) {
+        console.log('Usuarios Conectados >>', resp)
+    })
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+/*socket.emit('crearMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});*/
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+//Escuchar cambios de Usuarios cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    console.log('Personas:', personas);
+});
+
+//Mensajes Privados
+socket.on('mensajePrivado', function (mensaje) {
+    console.log('Mensaje Privado: >>', mensaje)
+});
